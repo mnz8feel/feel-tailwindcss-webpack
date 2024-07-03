c1f976047143b1e4092d040311c50031d7793c18
mnz8 <mnz8@foxmail.com>
2024-07-03 23:09:25 +0800

    feat: step2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import './style.css';
+
 const hello = document.createElement('h1');
+const wrapper = document.createElement('div');
 
 hello.textContent = 'Hello World!';
-document.body.appendChild(hello);
+hello.classList.add('bg-gradient-to-r', 'from-green-400', 'to-blue-500', 'text-transparent', 'text-9xl', 'bg-clip-text', 'font-extrabold');
+
+wrapper.classList.add('flex', 'w-full', 'h-screen', 'justify-center', 'items-center');
+
+wrapper.appendChild(hello);
+document.body.appendChild(wrapper);
