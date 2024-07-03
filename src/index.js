fc4e72b8d63c2e00ef749f6b71dbd6d95c17774a
mnz8 <mnz8@foxmail.com>
2024-07-03 23:14:53 +0800

    feat: 增加前缀

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const hello = document.createElement('h1');
 const wrapper = document.createElement('div');
 
 hello.textContent = 'Hello World!';
-hello.classList.add('bg-gradient-to-r', 'from-green-400', 'to-blue-500', 'text-transparent', 'text-9xl', 'bg-clip-text', 'font-extrabold');
+hello.classList.add('nx-bg-gradient-to-r', 'nx-from-green-400', 'nx-to-blue-500', 'nx-text-transparent', 'nx-text-9xl', 'nx-bg-clip-text', 'nx-font-extrabold');
 
-wrapper.classList.add('flex', 'w-full', 'h-screen', 'justify-center', 'items-center');
+wrapper.classList.add('nx-flex', 'nx-w-full', 'nx-h-screen', 'nx-justify-center', 'nx-items-center');
 
 wrapper.appendChild(hello);
 document.body.appendChild(wrapper);
