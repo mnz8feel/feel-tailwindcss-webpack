cab9dce7201c41c5f546047cd70f9365350ff2fd
mnz8 <mnz8@foxmail.com>
2024-07-29 00:39:05 +0800

    feat: 如何让 tailwindcss config file 生效

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 module.exports = {
   // Add you postcss configuration here
   // Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
   plugins: [['tailwindcss'], ['autoprefixer']],
+  // 指定 tailwindcss config 文件，需要使用 path
+  // tailwindcss: { config: path.join(__dirname, 'tailwind.config.js') },
 };
